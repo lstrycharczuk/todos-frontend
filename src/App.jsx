3830897df1c323b5141a6d4aaca9d226b8c9a7e0
Laura Strycharczuk <u21_laustr_lbn@technischools.com>
2024-03-26 14:21:16 +0100

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import 
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
+
+  const [retries, setRetries] = useState(0)
 
   useEffect(() => {
     fetch(`${BASE_API_URL}/todos`)
       .then(resp => resp.json())
       .then(data => {
         setTodos(data)
+      }).catch(err => {
+        console.error(err)
+        if (retries < 3) {
+          setRetries(retries + 1)
+        }
       })
-  }, [])
+  }, [retries])
 
   return (
-    <>
-      <div>
-        <h1>TODO list</h1>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo._id}>
-                  <span>{todo.task}</span>
-              </li>
-            ))}
-          </ul>
-      </div>
-    </>
-  );
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
